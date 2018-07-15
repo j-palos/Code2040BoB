b1fae72af61dbee80606de7f7bede1bdd27f6c21
Jesus Palos <jpalos@pandora.com>
2018-07-15 14:24:06 -0700

    Setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'reactstrap';
 
 class App extends Component {
 
-    
+
 
   render() {
     return (
